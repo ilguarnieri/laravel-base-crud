22495bcd376b83357e2119ae854cd769af1fe61a
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-05-07 22:37:40 +0200

    create project + migration + seeder

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics = config('comics');
+        
+        foreach ($comics as $el) {
+            
+            $comic = new Comic();
+
+            $comic->title = $el['title'];
+            $comic->description = $el['description'];
+            $comic->thumb = $el['thumb'];
+            $comic->price = $el['price'];
+            $comic->series = $el['series'];
+            $comic->sale_date = $el['sale_date'];
+            $comic->type = $el['type'];
+
+            $comic->save();
+        }
+    }
+}
